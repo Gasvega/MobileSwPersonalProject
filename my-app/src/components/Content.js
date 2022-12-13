f44e7bfd527bcc52564c9098798a16278ab3b07d
Gasvega <mji631@naver.com>
2022-12-14 06:00:05 +0900

    불필요한 폴더 및 파일 삭제

--- my-app/src/components/Content.js
+++
@@ -1,9 +0,0 @@
-const Content = (props) =>  (
-    <article>
-      <h2>{props.title}</h2>
-      {props.desc}
-    </article>
-  )
-
-
-  export default Content;
--- testFolder/ttest.js
+++
@@ -1,7 +0,0 @@
-var num1 = 1;
-var num2 = 2;
-let num3 = 3;
-var string1 = num1 + ' add ' + num2 + " = " + num3
-var string2 = `${num1} add ${num2} = ${num3}`
-console.log(string1)
-console.log(string2)
